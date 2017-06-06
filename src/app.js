ebc039ec5dc6deca98b6a8b7238c12b7c85c02d1
Adam Pasternack <adam.pasternack@gmail.com>
2017-06-05 23:16:26 -0400

    hooking up firebase

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyACzqugyfcGhJ_0wlQhN8w-b5053lqonW0',
+            authDomain: 'authentication-8b29b.firebaseapp.com',
+            databaseURL: 'https://authentication-8b29b.firebaseio.com',
+            projectId: 'authentication-8b29b',
+            storageBucket: 'authentication-8b29b.appspot.com',
+            messagingSenderId: '681883843255'
+        });
+    }
+
     render() {
         return (
             <View>
