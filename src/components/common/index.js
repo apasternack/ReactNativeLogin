c80d832fc814ed1b6587a87f35e33a2bcbb43ed0
Adam Pasternack <adam.pasternack@gmail.com>
2017-06-07 16:33:08 -0400

    making text input a reusable component: Passing value and onChange down as props

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,3 +2,4 @@ export * from './Button'; // this * notation imports and exports in one line
 export * from './Card';
 export * from './CardSection';
 export * from './Header';
+export * from './Input';
